a4350ab8eeae99741c72742bf816f647b1cb5a26
Jean-xavier <religieuxjxavier@gmail.com>
2020-10-06 17:09:21 +0200

    :arrow_up: feat : Correction flag cpp01 | ex00

--- cpp01/ex00/Pony.hpp
+++ cpp01/ex00/Pony.hpp
@@ -6,7 +6,7 @@
 /*   By: Jeanxavier <Jeanxavier@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/06 15:31:00 by Jeanxavier        #+#    #+#             */
-/*   Updated: 2020/10/06 16:30:33 by Jeanxavier       ###   ########.fr       */
+/*   Updated: 2020/10/06 17:05:24 by Jeanxavier       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ public:
 	int			weight;
 	int			age;
 
-	Pony(std::string name, std::string color, int weight, std::string unicorn, int age);
+	Pony(std::string name, std::string color, std::string unicorn, int weight, int age);
 	~Pony();
 	void	draw_pony(std::string unicorn);
 };
--- cpp01/ex00/main.cpp
+++ cpp01/ex00/main.cpp
@@ -6,23 +6,23 @@
 /*   By: Jeanxavier <Jeanxavier@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/06 15:31:03 by Jeanxavier        #+#    #+#             */
-/*   Updated: 2020/10/06 16:35:21 by Jeanxavier       ###   ########.fr       */
+/*   Updated: 2020/10/06 17:08:06 by Jeanxavier       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Pony.hpp"
 
-void	ponyOnTheStack(std::string name, std::string color, int weight,
-std::string unicorn, int age)
+void	ponyOnTheStack(std::string name, std::string color, std::string unicorn,
+int weight, int age)
 {
-	Pony	pony(name, color, weight, unicorn, age);
+	Pony	pony(name, color, unicorn, weight, age);
 	return ;
 }
 
-void	ponyOnTheHeap(std::string name, std::string color, int weight,
-std::string unicorn, int age)
+void	ponyOnTheHeap(std::string name, std::string color, std::string unicorn,
+int weight, int age)
 {
-	Pony	*pony = new Pony(name, color, weight, unicorn, age);
+	Pony	*pony = new Pony(name, color, unicorn,  weight, age);
 	delete pony;
 	return ;
 }
@@ -30,9 +30,9 @@ std::string unicorn, int age)
 
 int			main(void)
 {
-	ponyOnTheStack("Hercule", "Brown", 150, "Yes", 5);
-	ponyOnTheHeap("Medusa", "Green", 70, "No", 2);
-	ponyOnTheHeap("Thor", "Dark", 500, "No", 99);
-	ponyOnTheStack("Athena", "Pink", 10, "Yes", 20);
+	ponyOnTheStack("Hercule", "Brown", "Yes", 150, 5);
+	ponyOnTheHeap("Medusa", "Green", "No", 70, 2);
+	ponyOnTheHeap("Thor", "Dark", "No", 500, 99);
+	ponyOnTheStack("Athena", "Pink", "Yes", 10, 20);
 	return 0;
 }
