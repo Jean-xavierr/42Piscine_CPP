a99738d4345063dbc6c7b0e070f36d148ec2ffd3
Jean-xavier RELIGIEUX <religieuxjxavier@gmail.com>
2020-10-28 11:59:00 +0100

    :arrow_up: feat : Add cpp07 | ex01

---
+++ cpp07/ex01/iter.cpp
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   iter.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jereligi <jereligi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/10/28 11:28:39 by jereligi          #+#    #+#             */
+/*   Updated: 2020/10/28 11:58:29 by jereligi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+template <typename T>
+void	iter(T *tab, int size, void (*f)(T &tab))
+{
+	for (int i = 0; i < size; i++)
+		f(tab[i]);
+	return ;
+}
+
+template <typename T>
+void		funct_addOne(T &tab)
+{
+	tab += 1;
+}
+
+template <typename T>
+void		funct_capitalizer(T &tab)
+{
+	if (tab >= 'a' && tab <= 'z')
+		tab -= 32;
+}
+
+int			main(void)
+{
+	int		i;
+	int		tab[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+	char	tab2[6] = {"Hello"};
+	int		size = 10;
+
+	iter<int>(tab, size, &funct_addOne);
+	for (i = 0; i < size; i++)
+		std::cout << tab[i] << std::endl;
+	std::cout << std::endl;
+
+	iter<char>(tab2, 5, &funct_capitalizer);
+	for (i = 0; i < 5; i++)
+		std::cout << tab2[i];
+	std::cout << std::endl;
+	
+	return 0;
+}
