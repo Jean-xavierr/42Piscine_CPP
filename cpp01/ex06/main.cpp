78d782c070ef5a3a5b4322ccbcbd3e2e7a957d70
Jean-xavier <religieuxjxavier@gmail.com>
2020-11-16 13:53:18 +0100

    :arrow_up: feat : Update main cpp01 | ex06

--- cpp01/ex06/main.cpp
+++ cpp01/ex06/main.cpp
@@ -6,7 +6,7 @@
 /*   By: Jeanxavier <Jeanxavier@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/07 15:49:28 by Jeanxavier        #+#    #+#             */
-/*   Updated: 2020/10/07 16:44:12 by Jeanxavier       ###   ########.fr       */
+/*   Updated: 2020/11/16 13:52:58 by Jeanxavier       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,11 +25,15 @@ int main()
     }
     {
         Weapon club = Weapon("crude spiked club");
+        Weapon weapon = Weapon("Sword");
 
         HumanB jim("Jim");
         jim.setWeapon(club);
         jim.attack();
         club.setType("some other type of club");
         jim.attack();
+        jim.setWeapon(weapon);
+        jim.attack();
     }
+    return 0;
 }
